d3f4d1eb671afadca06f79a93cfde956d307dd87
Charliekofg <charliekofg@hotmail.com>
2026-02-10 06:16:38 +0000

    token generation proofed with Postman_basic login interface_tbd:continue the login function

---
+++ src/front/pages/Login.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react"
+import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+
+export const Login = () => {    
+
+    const { store, dispatch } = useGlobalReducer()
+
+    const loadMessage = async () => {
+        try {
+            const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+            if (!backendUrl) throw new Error("VITE_BACKEND_URL is not defined in .env file")
+
+            const response = await fetch(backendUrl + "/api/hello")
+            const data = await response.json()
+
+            if (response.ok) dispatch({ type: "set_hello", payload: data.message })
+
+            return data
+
+        } catch (error) {
+            if (error.message) throw new Error(
+                `Could not fetch the message from the backend.
+                Please check if the backend is running and the backend port is public.`
+            );
+        }
+
+    }
+
+    useEffect(() => {
+        loadMessage()
+    }, [])
+
+    return (
+        <div className="text-center mt-5">
+            <h1 className="display-4">Login</h1>
+            <div>
+                <input type="text"/>
+                <input type="password"/>
+                <button>Login</button>
+            </div>
+        </div>
+    );
+}; 
+
+export default Login
